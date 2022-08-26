36c9794c4236023db93e6d2ebb9d3de609b30757
Kouye-Valela <thickrabbiey@gmail.com>
2022-08-26 14:50:21 +0200

    first commit

---
+++ INDEX.js
@@ -0,0 +1,13 @@
+function validate() {
+    var  username = document.getElementById("EMAIL-ADDRESS").value ;
+    var  Password = document.getElementById("PASSWORD").value ;
+    if (username =='adim'&& password == 'user') {
+        alert("login succesfully")
+        
+    } else{
+
+        alert('login failed')
+
+    }
+    
+}
